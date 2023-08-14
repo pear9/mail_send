8e884502cf167c116c712870af356ab4eae229af
Anis Shrestha <nepal.dev7@asterinnovations.com>
2023-08-14 21:04:44 +0545

    update credentiials

--- flask3.py
+++ flask3.py
@@ -15,12 +15,7 @@ app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', False) in ['True', '
 
 
 
-# app.config['MAIL_SERVER']='smtp.gmail.com'
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USERNAME'] = 'rosepear9@gmail.com'
-# app.config['MAIL_PASSWORD'] = 'etbntlbbucxnhlre'
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_USE_SSL'] = True
+
 mail = Mail(app)
 
 # message object mapped to a particular URL ‘/’
