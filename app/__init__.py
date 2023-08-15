745f42bf0736bf97cb9ba88a9d1eed97cb348a2d
Anis Shrestha <nepal.dev7@asterinnovations.com>
2023-08-15 11:28:06 +0545

    update on getting form body

--- app/__init__.py
+++ app/__init__.py
@@ -18,22 +18,54 @@ app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true'
 
 
 
+
+
+
 mail = Mail(app)
 
 # message object mapped to a particular URL ‘/’
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/send_email',method=['GET','POST'])
+def send_email():
+    if request.method=='POST':
+        recipient_email = 'tesewa9275@v1zw.com'
+        recipient_name = 'Recipient Name'
+        data = request.form
+        name = data.get('name')
+        email = data.get('email')
+        subject = 'Your Subject Here'
+        sender_email = app.config('MAIL_USERNAME')
+
+        message = Message(subject, sender=sender_email, recipients=[recipient_email])
+        message.html = render_template('email_template.html', cust_name=name,cust_email=email,message=message)
+
+        try:
+            mail.send(message)
+            return 'Email sent successfully!'
+        except Exception as e:
+            return str(e)
+    else:
+        return render_template('index.html')
+    
 @app.route("/send",methods=['GET','POST'])   
 def send():
     if request.method == 'POST':
-        msg = Message(
-                        'Response Got',
-                        sender ='rosepear9@gmail.com',
-                        recipients = ['sijanthpa@gmail.com']
-                    )
-        msg.body = 'Hello Flask message sent from Flask-Mail'
-        mail.send(msg)
+        data = request.form
+        name = data.get('name')
+        email = data.get('email')
+        message=data.get('message')
+        # Do something with the received data
+        print(f"Received data: Name={name}, Email={email},Message={message}")
+        # msg = Message(
+        #                 'Response Got',
+        #                 sender ='rosepear9@gmail.com',
+        #                 recipients = ['tesewa9275@v1zw.com']
+        #             )
+        # msg.body = 'Hello Flask message sent from Flask-Mail'
+        # mail.send(msg)
         return 'Sent'
     else:
         return render_template('index.html')
