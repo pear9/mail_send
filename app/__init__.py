6694b6cae200f4634aba037f5f5eef722a00cb76
Anis Shrestha <nepal.dev7@asterinnovations.com>
2023-08-15 10:49:35 +0545

    added templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,25 @@
 # importing libraries
 import os
-from flask import Flask
+from flask import Flask ,render_template,request, redirect
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 mail = Mail(app) # instantiate the mail class
 
 # configuration of mail
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) in ['True', 'true', '1']
-app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true', '1']
+# app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+# app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+# app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+# app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) in ['True', 'true', '1']
+# app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true', '1']
 
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USERNAME = 'rosepear9@gmail.com'
+MAIL_PASSWORD = 'etbntlbbucxnhlre'
+MAIL_USE_SSL = True
 
 
 
@@ -22,14 +28,20 @@ mail = Mail(app)
 # message object mapped to a particular URL ‘/’
 @app.route("/")
 def index():
-    msg = Message(
-                    'Hello',
-                    sender ='rosepear9@gmail.com',
-                    recipients = ['sijanthpa@gmail.com']
-                )
-    msg.body = 'Hello Flask message sent from Flask-Mail'
-    mail.send(msg)
-    return 'Sent'
+    return render_template('index.html')
+@app.route("/send",methods=['POST'])   
+def send():
+    if request.method == 'POST':
+        msg = Message(
+                        'Response Got',
+                        sender ='rosepear9@gmail.com',
+                        recipients = ['sijanthpa@gmail.com']
+                    )
+        msg.body = 'Hello Flask message sent from Flask-Mail'
+        mail.send(msg)
+        return 'Sent'
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
