e02c6c0013b5281593f40ea016bc012f8e58da43
Anis Shrestha <nepal.dev7@asterinnovations.com>
2023-08-15 10:02:28 +0545

    env variable

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 mail = Mail(app) # instantiate the mail class
 
 # configuration of mail
-app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT'))
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', False) in ['True', 'true', '1']
-app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', False) in ['True', 'true', '1']
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) in ['True', 'true', '1']
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true', '1']
 
 
 
