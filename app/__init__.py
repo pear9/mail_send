dcfb7da8123692036ace25ecc45051c5bb97c524
Anis Shrestha <nepal.dev7@asterinnovations.com>
2023-08-15 10:50:22 +0545

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,15 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 mail = Mail(app) # instantiate the mail class
 
-# configuration of mail
-# app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-# app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-# app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-# app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-# app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) in ['True', 'true', '1']
-# app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true', '1']
+#configuration of mail
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) in ['True', 'true', '1']
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) in ['True', 'true', '1']
 
 
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USERNAME = 'rosepear9@gmail.com'
-MAIL_PASSWORD = 'etbntlbbucxnhlre'
-MAIL_USE_SSL = True
 
 
 
